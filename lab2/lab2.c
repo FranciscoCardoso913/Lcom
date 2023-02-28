1ba1b9c8b76dc9583b5e83abd7ef003437496a5b
ZeAntonioM <joseapmartins2003@gmail.com>
2023-02-28 17:35:39 +0000

    timer_set_frequency done and tested

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -51,10 +51,13 @@ int(timer_test_read_config)(uint8_t timer, enum timer_status_field field) {
 }
 
 int(timer_test_time_base)(uint8_t timer, uint32_t freq) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  
+  if (timer < 0 || timer > 2) {
+    printf("Invalid timer number! \n"); 
+    return 1;
+  }
 
-  return 1;
+  return timer_set_frequency(timer, freq);
 }
 
 int(timer_test_int)(uint8_t time) {
--- lab2/timer.c
+++ lab2/timer.c
@@ -6,10 +6,58 @@
 #include "i8254.h"
 
 int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  
+  uint16_t counter = TIMER_FREQ/freq;
+  uint8_t st, lsb, msb, port; 
 
-  return 1;
+  if(timer_get_conf(timer, &st)) {
+    printf("Error reading the configuration");
+    return 1;
+  }
+
+  uint8_t mask = (BIT(3)|BIT(2)|BIT(1)|BIT(0));
+
+  uint8_t ctrl = (st & mask) | TIMER_LSB_MSB;
+
+  switch (timer) {
+    case 0:
+      port = TIMER_0; break;
+    case 1:
+      ctrl |= TIMER_SEL1;
+      port = TIMER_1; break;
+
+    case 2:
+      ctrl |= TIMER_SEL2;
+      port = TIMER_2; break;
+  }
+
+  if (sys_outb(TIMER_CTRL, ctrl) != OK) {
+    printf("Error writing to the timer control register! \n");
+    return 1;
+  }
+
+  if(util_get_LSB(counter, &lsb)){
+    printf("Error getting the LSB! \n");
+    return 1;
+  }
+
+  if(util_get_MSB(counter, &msb)){
+    printf("Error getting the MSB! \n");
+    return 1;
+  }
+
+  if (sys_outb(port, lsb) != OK) {
+    printf("Error writing to the timer 0 register! \n");
+    return 1;
+  }
+  if (sys_outb(port, msb) != OK) {
+    printf("Error writing to the timer 0 register! \n");
+    return 1;
+  }
+
+  
+
+  return 0;
 }
 
 int (timer_subscribe_int)(uint8_t *bit_no) {
