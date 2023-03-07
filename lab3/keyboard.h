b6d2ab194048c0382c9c409b96adb224a3f6f2fa
marco-vb <marcofilipe03@hotmail.com>
2023-03-07 09:48:21 +0000

    cleaned header file for keyboard

--- lab3/keyboard.h
+++ lab3/keyboard.h
@@ -18,39 +18,26 @@ int hook_id;
 // return value used in keyboard.c
 int ret;
 
-#define KBD_IRQ 1
-#define READ_CMD_BYTE 0x20
-#define WRITE_CMD_BYTE 0x60
-#define KBD_INT 0x01
-#define ESC_BREAK 0x81
-
+// delay for polling
 #define DELAY_US 20000
 
-#define STAT_REG 0x64
-#define KBC_CMD_REG 0x64
-#define OUT_BUF 0x60
-#define IN_BUF 0x60
+// interrupt line for keyboard
+#define KBD_IRQ 1
 
-#define OBF BIT(0)
-#define IBF BIT(1)
+// useful ports
+#define ESC_BREAK 0x81
+#define STAT_REG 0x64
+#define OUT_BUF 0x60
+
+// errors: parity and timeout
 #define PAR_ERR BIT(7)
 #define TO_ERR BIT(6)
 
-#define KBC_EN BIT(0)
-#define KBC_INT BIT(1)
+// make or break bit
 #define MAKE_BIT BIT(7)
 
+// if two byte code then first byte is 0xE0
 #define TWO_BYTE_CODE 0xE0
-
-#define ESC_BREAK_CODE 0x81
-
-#define KBC_READ_CMD 0x20
-#define KBC_WRITE_CMD 0x60
-
-#define KBC_DIS BIT(4)
-
-#define RESEND 0xFE
-#define ERROR 0xFC
 
 int (kbd_subscribe_int)(uint8_t *bit_no);
 int (kbd_unsubscribe_int)();
